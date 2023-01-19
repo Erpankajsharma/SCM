f8c132d2043f613ac839750ab34572621c0bfd29
Me <Me@DESKTOP-HM27A8M>
2023-01-19 13:07:36 +0530

    change u

---
+++ src/test/java/practice_maven/ChangeReq.java
@@ -0,0 +1,12 @@
+package practice_maven;
+
+import org.testng.annotations.Test;
+
+public class ChangeReq {
+	
+	@Test
+	public void createNewChange() {
+		System.out.println("created new");
+	}
+
+}
